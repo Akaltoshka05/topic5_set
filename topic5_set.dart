943c490996a13642aa740077842e3f56c687d23b
Akniyet <131252267+Akaltoshka05@users.noreply.github.com>
2023-11-04 15:12:46 +0600

    Add files via upload

---
+++ topic5_set.dart
@@ -0,0 +1,72 @@
+//[Task 5]
+//Напишите программу Dart, 
+//чтобы проверить, является ли 
+//набор подмножеством другого набора.
+//void main (){
+//    set permno = {"Ako", "Vasya", "Anna", "Mike"};     
+//    set vtomno = {"Ako", "Vasya","Sam", "Kate"};   
+//    var set = permno.intersection(vtomno);   
+//    print(set);  //true если набор является подмножеством другого набора
+//}
+
+
+
+//Напишите программу Dart для удаления 
+//всех элементов из заданного набора.
+//void main (){
+//    var set = {"Tom", "Alice", "Bob", "Sam", "Kate"};
+//    set.removeAll({"Tom", "Alice", "Bob", "Sam", "Kate"});
+
+//    print(set);  
+//}
+
+
+
+//Напишите программу Dart, чтобы найти длину набора.
+//void main (){
+//    var set = {"Tom", "Bob", "Sam"};
+//    print("Length: ${set.length}");
+//}
+
+
+
+//Напишите программу Dart, 
+//чтобы проверить, присутствует 
+//ли заданное значение в наборе или нет.
+//void main (){
+//    var set = {"Tom", "Bob", "Sam"};
+//    print(set.contains("Akniyet"));    // false
+//}
+
+
+
+//Напишите программу Dart, чтобы проверить, 
+//не имеют ли два заданных набора общих 
+//элементов.
+//void main (){
+//    var first = {"1", "2", "3", "4"};     
+//    var second = {"1", "2","5", "6"};  
+//    var set = first.intersection(second);  
+//    print(set);  
+//}
+
+
+
+//Напишите программу Dart для поиска элементов 
+//в заданном наборе, которых нет в другом наборе.
+//void main (){
+//  var dart = {"Tom", "Alice", "Bob", "Mike"};     
+//var flutter = {"Tom", "Alice"};   
+//var set = dart.difference(flutter);  
+//print(set);        
+//}
+
+
+//Напишите программу Dart, 
+// чтобы удалить пересечение второго набора с первым набором.
+//void main(){
+//    var dart = {"Tom", "Alice", "Bob", "Mike"};     
+//     var flutter = {"Tom", "Alice"};   
+//     var set = dart.difference(flutter);  
+//     print(set);  
+//}
