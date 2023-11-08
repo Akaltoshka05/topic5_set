9c0dac140acf28e9d6730eba0988f78d57d8f0bb
Akniyet <131252267+Akaltoshka05@users.noreply.github.com>
2023-11-08 11:38:15 +0600

    Update topic5_set.dart

--- topic5_set.dart
+++ topic5_set.dart
@@ -9,6 +9,23 @@
 //    print(set);  //true если набор является подмножеством другого набора
 //}
 
+//ili osy variant bar
+//void main() {
+//  Set<String> permno = {"Ako", "Vasya", "Anna", "Mike"};
+//  Set<String> vtomno = {"Ako", "Vasya", "Sam", "Kate"};
+  
+//  bool isSubset = isSubsetOf(permno, vtomno);
+
+//  if (isSubset) {
+//    print('permno is a subset of vtomno');
+//  } else {
+//    print('permno is not a subset of vtomno');
+//  }
+//}
+
+//bool isSubsetOf(Set<String> subset, Set<String> superset) {
+//  return superset.containsAll(subset);
+//}
 
 
 //Напишите программу Dart для удаления 
@@ -43,30 +60,37 @@
 //Напишите программу Dart, чтобы проверить, 
 //не имеют ли два заданных набора общих 
 //элементов.
-//void main (){
-//    var first = {"1", "2", "3", "4"};     
-//    var second = {"1", "2","5", "6"};  
-//    var set = first.intersection(second);  
-//    print(set);  
+//void main() {
+//  var first = {"1", "2", "3", "4"};
+//  var second = {"1", "2", "5", "4"};
+//  var intersection = first.intersection(second);
+//  
+//  if (intersection.isEmpty) {
+//    print("Нет общих элементов между наборами.");
+//  } else {
+//  }
 //}
 
 
 
 //Напишите программу Dart для поиска элементов 
 //в заданном наборе, которых нет в другом наборе.
-//void main (){
-//  var dart = {"Tom", "Alice", "Bob", "Mike"};     
-//var flutter = {"Tom", "Alice"};   
-//var set = dart.difference(flutter);  
-//print(set);        
+//void main() {
+//  var dart = {"Tom", "Alice", "Bob", "Mike"};
+//  var flutter = {"Tom", "Alice"};
+//  var difference = dart.difference(flutter).toList();
+
+//  print("Элементы в dart, которых нет в flutter: $difference");
 //}
+
 
 
 //Напишите программу Dart, 
 // чтобы удалить пересечение второго набора с первым набором.
-//void main(){
-//    var dart = {"Tom", "Alice", "Bob", "Mike"};     
-//     var flutter = {"Tom", "Alice"};   
-//     var set = dart.difference(flutter);  
-//     print(set);  
+//void main() {
+//  var dart = {"Tom", "Alice", "Bob", "Mike"};
+//  var flutter = {"Tom", "Alice"};
+//  var set = dart.difference(flutter);
+
+//  print(set);
 //}
